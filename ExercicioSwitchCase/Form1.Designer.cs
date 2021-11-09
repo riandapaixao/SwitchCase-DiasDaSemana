426d99ebabfd328c6deeedc9c4518b5ae07c4b17
Rian <Rian@DESKTOP-RTHTRUH>
2021-11-09 17:26:09 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioSwitchCase/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace ExercicioSwitchCase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.textBoxDia = new System.Windows.Forms.TextBox();
+            this.buttonVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(175, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dia Da Semana";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(143, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dia:";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.Location = new System.Drawing.Point(232, 227);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(16, 24);
+            this.labelResultado.TabIndex = 2;
+            this.labelResultado.Text = "-";
+            // 
+            // textBoxDia
+            // 
+            this.textBoxDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDia.Location = new System.Drawing.Point(191, 107);
+            this.textBoxDia.Name = "textBoxDia";
+            this.textBoxDia.Size = new System.Drawing.Size(100, 29);
+            this.textBoxDia.TabIndex = 3;
+            // 
+            // buttonVerificar
+            // 
+            this.buttonVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonVerificar.Location = new System.Drawing.Point(191, 166);
+            this.buttonVerificar.Name = "buttonVerificar";
+            this.buttonVerificar.Size = new System.Drawing.Size(100, 37);
+            this.buttonVerificar.TabIndex = 5;
+            this.buttonVerificar.Text = "Verificar";
+            this.buttonVerificar.UseVisualStyleBackColor = true;
+            this.buttonVerificar.Click += new System.EventHandler(this.Verificar);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 298);
+            this.Controls.Add(this.buttonVerificar);
+            this.Controls.Add(this.textBoxDia);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.TextBox textBoxDia;
+        private System.Windows.Forms.Button buttonVerificar;
+    }
+}
+
---
+++ ExercicioSwitchCase/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercicioSwitchCase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Verificar(object sender, EventArgs e)
+        {
+            switch (textBoxDia.Text)
+            {
+                case "1":
+                    labelResultado.Text = "Domingo";
+                    break;
+                case "2":
+                    labelResultado.Text = "Segunda-Feira";
+                    break;
+                case "3":
+                    labelResultado.Text = "Terça-Feira";
+                    break;
+                case "4":
+                    labelResultado.Text = "Quarta-Feira";
+                    break;
+                case "5":
+                    labelResultado.Text = "Quinta-Feira";
+                    break;
+                case "6":
+                    labelResultado.Text = "Sexta-Feira";
+                    break;
+                case "7":
+                    labelResultado.Text = "Sábado";
+                    break;
+                default:
+                    labelResultado.Text = "Dia Inválido";
+                    break;
+
+            }
+            /*if(textBoxDia.Text.Equals("1")){
+                labelResultado.Text = "Domingo";
+
+            }else if (textBoxDia.Text.Equals("2")){
+                labelResultado.Text = "Segunda-Feira";
+            }else if (textBoxDia.Text.Equals("3")){
+                labelResultado.Text = "Terça-Feira";
+            }else if (textBoxDia.Text.Equals("4")) {
+                labelResultado.Text = "Quarta-Feira";
+            }else if (textBoxDia.Text.Equals("5")) {
+                labelResultado.Text = "Quinta-Feira";
+            }else if (textBoxDia.Text.Equals("6")) {
+                labelResultado.Text = "Sexta-Feira";
+            }else if (textBoxDia.Text.Equals("7")) {
+                labelResultado.Text = "Sábado";
+            }else {
+                labelResultado.Text = "Dia Inválido";
+             }*/
+        }
+    }
+}
